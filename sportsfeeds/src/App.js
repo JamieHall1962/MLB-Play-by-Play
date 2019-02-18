a9d24bd21d6879f416815189860562c4515876c4
Jamie Hall <hall.jamie@gmail.com>
2019-02-17 22:37:04 -0500

    Data cleaner complete except for edge cases

--- sportsfeeds/src/App.js
+++ sportsfeeds/src/App.js
@@ -1,130 +1,207 @@
 import React, { Component } from "react";
+import  Table from "react-bootstrap/Table";
 
 import "./App.css";
 import axios from "axios";
-import Gamelog from "./Plays"; // This is because I eventually intend to send down the data to a presentational component
+import Gamelog from "./Plays";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      pBp: {}, // originally had all data going here
-      results: [], // later tried breaking it down into individual arrays
+      pBp: {},
+      results: [],
       batterId: [],
       batterName: [],
-      BatterLR: [],
+      batterLR: [],
       pitcherId: [],
       pitcherName: [],
       pitcherLR: []
     };
   }
-
+  // 2018/10/01/milmlb-chnmlb-1R
   componentDidMount() {
+    const gameId = "20180608-kc-oak"
+    
     const requestOptions = {
       headers: {
         Authorization:
-          "Basic " + btoa(`a9e283b4-6f81-46f7-b2e8-6bd635:MYSPORTSFEEDS`) // had this in an .env file but hard coded it in in case you need to look for yourself
+          "Basic " + btoa(`a9e283b4-6f81-46f7-b2e8-6bd635:MYSPORTSFEEDS`)
       }
     };
     axios
       .get(
-        "https://api.mysportsfeeds.com/v2.0/pull/mlb/2018-regular/games/20180514-CLE-DET/playbyplay.json",
+        `https://api.mysportsfeeds.com/v2.0/pull/mlb/2018-regular/games/${gameId}/playbyplay.json`,
         requestOptions
       )
 
       .then(res => {
-        this.setState({ pBp: res.data }); // leftover from having all data in the pBp object in state. A console.log here shows all data
+        this.setState({ pBp: res.data });
 
-        const allData = res.data; // as does this
+        const allData = res.data;
+        console.log(allData);
 
-        const tempResults = []; // this little section breaks out data into individual arrays. I deleted all but two because that will give you an idea and the other 5 arrays are the same idea
+        const tempResults = [];
         const tempBatterId = [];
+        const tempBatterName = [];
+        const tempBatterLR = [];
+        const tempPitcherId = [];
+        const tempPitcherName = [];
+        const tempPitcherLR = [];
+
         for (let x = 0; x < allData.atBats.length; x++) {
-          tempResults.push(allData.atBats[x].atBatPlay[0].batterUp.result);
+          allData.atBats[x].atBatPlay[0].batterUp.result !== null
+            ? tempResults.push(allData.atBats[x].atBatPlay[0].batterUp.result)
+            : tempResults.push("*** NO PLAY ***");
           tempBatterId.push(
             allData.atBats[x].atBatPlay[0].batterUp.battingPlayer.id
           );
+
+          tempBatterName.push(
+            `${
+              allData.atBats[x].atBatPlay[0].batterUp.battingPlayer.firstName
+            } ${allData.atBats[x].atBatPlay[0].batterUp.battingPlayer.lastName}`
+          );
+
+          tempBatterLR.push(
+            allData.atBats[x].atBatPlay[0].batterUp.standingLeftOrRight
+          );
+
+          for (let y = 0; y < allData.atBats[x].atBatPlay.length; y++) {
+            try {
+              tempPitcherLR.push(
+                allData.atBats[x].atBatPlay[y + 1].pitch.throwingLeftOrRight
+              );
+              tempPitcherId.push(
+                allData.atBats[x].atBatPlay[y + 1].pitch.pitchingPlayer.id
+              );
+              tempPitcherName.push(
+                `${
+                  allData.atBats[x].atBatPlay[y + 1].pitch.pitchingPlayer
+                    .firstName
+                } ${
+                  allData.atBats[x].atBatPlay[y + 1].pitch.pitchingPlayer
+                    .lastName
+                }`
+              );
+              console.log("X before break ", x, tempPitcherLR[x]);
+              break;
+            } catch (err) {
+              if (y + 1 === allData.atBats[x].atBatPlay.length) {
+                tempPitcherLR.push("IRR");
+                tempPitcherId.push(0);
+                tempPitcherName.push("IRRELEVANT");
+              } else {
+                continue;
+              }
+            } finally {
+              console.log("X in finally  ", x, tempPitcherLR[x]);
+            }
+          }
         }
         this.setState({
-          // sets state with an object of arrays. Yet another failed attempt
-          pBp: {
-            ...this.state.pBp,
-            results: tempResults,
-            batterId: tempBatterId
-          }
+          results: tempResults,
+          batterId: tempBatterId,
+          batterName: tempBatterName,
+          batterLR: tempBatterLR,
+          pitcherId: tempPitcherId,
+          pitcherName: tempPitcherName,
+          pitcherLR: tempPitcherLR
         });
-        console.log(
-          // returns arrays with expected results. Or maybe I'm reading them wrong. Could be.
-          "State holds: ",
-          this.state.pBp.results,
-          this.state.pBp.batterId
-        );
-        console.log("Object values:  ", Object.values(this.state.pBp.results)); // part of an attempt to use objects. Works here. Fails in rendering
       })
       .catch(err => console.log(err));
   }
 
   render() {
-    this.state.pBp.atBats && console.log("Hello", this.state.pBp.atBats); // returns an array
-
-    // this next section renders the data that I need. It works, which shows I can get to the needed data. But I need to find a way to map over it because there are a variable number of at-bats in any given game. This particular at bat was #18 of 75. If you change let ab=18 to another number <=75 you will see other at-bats from this random game from last May
-
-    let ab = 26;
+    // let ab = 26; used to test a single at-bat
     return (
-      <div>
-        <p>
-          {`Result:      `}
-          {this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[0].batterUp.result}
-        </p>
-        <p>
-          {`Batter (L/R): `}
-          {this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[0].batterUp.standingLeftOrRight}
-        </p>
-        <p>
-          {`Batter ID: `}
-          {this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer.id}
-        </p>
-        <p>
-          {`Batter Name: `}
-          {`${this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer
-              .firstName} 
-             ${this.state.pBp.atBats &&
-               this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer
-                 .lastName}`}
-        </p>
-        <p>
-          {`Pitcher ID: `}
-          {this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer.id}
-        </p>
-        <p>
-          {`Pitcher Name: `}
-          {` ${this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer
-              .firstName} 
-            ${this.state.pBp.atBats &&
-              this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer
-                .lastName}`}
-        </p>
-        <p>
-          {`Pitcher (L/R): `}
-          {this.state.pBp.atBats &&
-            this.state.pBp.atBats[ab].atBatPlay[1].pitch.throwingLeftOrRight}
-        </p>
+      // <div>
+      //   <p>
+      //     {`Result:      `}
+      //     {this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[0].batterUp.result}
+      //   </p>
+      //   <p>
+      //     {`Batter (L/R): `}
+      //     {this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[0].batterUp.standingLeftOrRight}
+      //   </p>
+      //   <p>
+      //     {`Batter ID: `}
+      //     {this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer.id}
+      //   </p>
+      //   <p>
+      //     {`Batter Name: `}
+      //     {`${this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer
+      //         .firstName}
+      //        ${this.state.pBp.atBats &&
+      //          this.state.pBp.atBats[ab].atBatPlay[0].batterUp.battingPlayer
+      //            .lastName}`}
+      //   </p>
+      //   <p>
+      //     {`Pitcher ID: `}
+      //     {this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer.id}
+      //   </p>
+      //   <p>
+      //     {`Pitcher Name: `}
+      //     {` ${this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer
+      //         .firstName}
+      //       ${this.state.pBp.atBats &&
+      //         this.state.pBp.atBats[ab].atBatPlay[1].pitch.pitchingPlayer
+      //           .lastName}`}
+      //   </p>
+      //   <p>
+      //     {`Pitcher (L/R): `}
+      //     {this.state.pBp.atBats &&
+      //       this.state.pBp.atBats[ab].atBatPlay[1].pitch.throwingLeftOrRight}
+      //   </p>
 
-        {/* end of section breaking out one at-bat. 
-        
-        Here is where the map function needs to come into play. The map attempt here fails miserably and I have tried literally hundreds of permutations including mapping Object.values and Object.keys. The 'this.state.pBp.atBats && ' is there because before I added it, the program was trying to render before the data was ready. If you comment out the next little section (before the </div>) you will see an individual at-bat rendered, otherwise an error will be generated. This one will give 'Uncaught TypeError: Cannot read property 'map' of undefined' but other attempts have given different errors */}
+      <>
+        {/* <Container className="mt-5 pt-5"> */}
+        {/* <Row className="card-columns"> */}
 
-        {/* {this.state.pBp.atBats &&
-          this.state.results.map((ab, i) => {
-            <Gamelog key={i} results={ab} />;
-          })} */}
-      </div>
+        <Table hover striped size="sm">
+          <thead>
+            <tr>
+              <th>PA #</th>
+              <th>Batter</th>
+              <th>(L/R)</th>
+              <th>Batter Id</th>
+              <th>Pitcher</th>
+              <th>(L/R)</th>
+              <th>Pitcher Id</th>
+              <th>Result</th>
+            </tr>
+          </thead>
+
+          {this.state.pBp.atBats &&
+            this.state.results.map((ab, i) => (
+              // <Col key={i} xs="3" style={{ marginBottom: "1rem" }}>
+              <tbody key={i}>
+                <tr>
+                  <Gamelog
+                    key={i}
+                    counter={i}
+                    result={ab}
+                    batterId={this.state.batterId[i]}
+                    batterName={this.state.batterName[i]}
+                    batterLR={this.state.batterLR[i]}
+                    pitcherId={this.state.pitcherId[i]}
+                    pitcherName={this.state.pitcherName[i]}
+                    pitcherLR={this.state.pitcherLR[i]}
+                  />
+                  {/* </Col> */}
+                </tr>
+              </tbody>
+            ))}
+        </Table>
+        {/* </Row> */}
+        {/* </Container> */}
+      </>
     );
   }
 }
--- sportsfeeds/src/Plays.js
+++ sportsfeeds/src/Plays.js
@@ -1,16 +1,45 @@
 import React from "react";
 
+import "./plays.css";
+
 const Gamelog = props => {
-    
-    console.log("Got here", {props})
-    return (
-        <>
-        
-    <h5>{props.result}</h5>
-  {/*   <h5>{props.batterId}</h5>
-    <h5>{props.gotHere}</h5> */}
+  return (
+    <>
+      
+          {/* <Card
+            className="shadow"
+            style={{ marginBottom: "1rem", borderRadius: "12" }}
+          >
+            <CardBody>
+              <CardTitle>
+                <h5> {props.result}</h5>{" "}
+              </CardTitle>
+
+              <CardSubtitle>
+                {" "}
+                {`${props.batterName} (${props.batterLR})`}{" "}
+              </CardSubtitle>
+            </CardBody>
+
+            <CardFooter style={{ color: "#5A6169", marginBottom: "1rem" }}>
+              {`${props.pitcherName} (${props.pitcherLR}) ${props.counter}`}{" "}
+            </CardFooter>
+          </Card> */}
+
+<th scope="row">{props.counter}</th>
+            <td>{props.batterName}</td>
+            <td>{props.batterLR}</td>
+            <td>{props.batterId}</td>
+            <td>{props.pitcherName}</td>
+            <td>{props.pitcherLR}</td>
+            <td>{props.pitcherId}</td>
+            
+            <td style={{ backgroundColor: props.result === '*** NO PLAY ***' ? 'red': 'none'}}> {props.result} </td>
+          
+           
+       
     </>
-    )
-}
+  );
+};
 
 export default Gamelog;
